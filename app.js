86fd19160f4252c4faa08b9e86494fd0fc6e0356
Sharmila Jesupaul <tosharmila@gmail.com>
2016-03-18 16:21:31 -0700

    responsive viewport

--- app.js
+++ app.js
@@ -1,35 +1,70 @@
-(function() {
+var data = [5, 10, 15, 20, 25];
+var width = 400;
+var height = 400;
 
-  var dataset = _.map(_.range(18), function(i) {
-    return Math.random() * 500;
-  });
+var xScale = d3.scale.ordinal()
+  .domain(data)
+  .rangeBands([0, width], 0.1, 0);
 
-  var w = 400;
-  var h = 300;
+var yScale = d3.scale.linear()
+  .domain([0, d3.max(data)])
+  .range([0, height]);
 
-  var yScale = d3.scale.linear()
-    .domain([0, d3.max(dataset) * 1.1]) // domain range from 0 to the largest possible data point
-    .range([0, h]);
-
-  var svg = d3.select('#chartArea')
-    .append('svg')
-    .attr('width', w)
-    .attr('height', h);
-
-  svg.selectAll('rect')
-    .data(dataset)
+function init(selection) {
+  selection.selectAll('rect')
+    .data(data)
     .enter()
     .append('rect')
-    .attr('class', 'bar')
+    .style('fill', 'green')
     .attr('x', function(d, i) {
-      return i * 22;
+      return xScale(d);
     })
     .attr('y', function(d) {
-      return h - yScale(d);
+      return height - yScale(d);
     })
-    .attr('width', 20)
+    .attr('width', xScale.rangeBand())
     .attr('height', function(d) {
       return yScale(d);
     });
+}
 
-})();
+function resizeSVG(w, h) {
+  d3.select('#chart svg')
+    .attr('width', w)
+    .attr('height', h);
+}
+
+// http://www.brendansudol.com/posts/responsive-d3/
+function responsivefy(svg) {
+  // get container + svg aspect ratio
+  var container = d3.select(svg.node().parentNode),
+    width = parseInt(svg.style('width')),
+    height = parseInt(svg.style('height')),
+    aspect = width / height;
+
+  // add viewBox and preserveAspectRatio properties,
+  // and call resize so that svg resizes on inital page load
+  svg.attr('viewBox', '0 0 ' + width + ' ' + height)
+    .attr('preserveAspectRatio', 'xMinYMid')
+    .call(resize);
+
+  // to register multiple listeners for same event type,
+  // you need to add namespace, i.e., 'click.foo'
+  // necessary if you call invoke this function for multiple svgs
+  // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+  d3.select(window).on('resize.' + container.attr('id'), resize);
+
+  // get width of container and resize svg to fit it
+  function resize() {
+    var targetWidth = parseInt(container.style('width'));
+    svg.attr('width', targetWidth);
+    svg.attr('height', Math.round(targetWidth / aspect));
+  }
+}
+
+d3.select('#chart')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .call(init)
+  .call(responsivefy);
